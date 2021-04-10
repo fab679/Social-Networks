6a066edbf70979f292835474a239160a091a9336
fab679 <kamaufabisch1175@gmail.com>
2021-04-10 13:05:16 +0300

    my first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const { Neo4jGraphQL } = require('@neo4j/graphql')
+const Neo4j = require('neo4j-driver')
+const { ApolloServer } = require('apollo-server')
+
+const driver = Neo4j.driver(
+    'bolt://34.200.223.121:7687',
+    Neo4j.auth.basic('neo4j', 'fixture-elbow-purposes')
+)
+
+
+
+const typeDefs = `
+type Post {
+    id:ID! @id
+    content: String!
+    createdAt:DateTime! @timestamp
+    creator:User! @relationship(type:"POSTED", direction:IN)
+    liked:[User] @relationship(type:"LIKES", direction:IN)
+}
+type User{
+    id:ID!@id
+    name:String
+    posts:[Post] @relationship(type:"POSTED", direction:OUT)
+    likes:[Post] @relationship(type:"LIKES", direction:OUT)
+    follows:[User] @relationship(type:"FOLLOWS", direction:OUT)
+}
+`;
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver })
+
+
+const server = new ApolloServer({
+    schema: neoSchema.schema,
+    context: ({ req }) => ({ req }),
+});
+
+server.listen(4000).then(() => console.log('Online'));
